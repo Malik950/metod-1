4229f1c363018662c096cd497692c44f71c48cb3
Malik Lomaliev <Maliklomaliev@outlook.com>
2019-09-24 13:46:12 +0200

    metod 1

---
+++ metod 1/metod 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace metod_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+
+        }
+        static double CirkelOmkrets(double r)
+        {
+            return (r * 2 * Math.PI);
+        }
+
+        static double CirkelArea(double r)
+        {
+            return (r * r * Math.PI);
+        }
+    }
+
+}
